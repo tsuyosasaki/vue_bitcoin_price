fd9021d3798d4631c5f68ce8f1673106090acea8
tsuyosasaki <tsuyo.sasaki@gmail.com>
2019-01-17 14:34:26 +0900

    create bitcoin price product

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    bpi: null,
+    hasError: false,
+    loading: true
+  },
+  mounted: function() {
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(function(response) {
+        this.bpi = response.data.bpi;
+        console.log(this.bpi);
+      }.bind(this))
+      .catch(function(error) {
+        console.log(error);
+        this.hasError = true;
+      }.bind(this))
+      .finally(function(){
+        this.loading = false;
+      }.bind(this));
+  },
+  filters: {
+    currencyDecimal(value) {
+      return value.toFixed(2);
+    }
+  }
+});
